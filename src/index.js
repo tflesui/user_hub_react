2464fb780b1ae60f24be4b54e5d951d196fa1cef
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-06-13 10:53:08 -0700

    Finished basic setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div id="App">
+      <h1>Sup, World</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
