80ea60559897f74b7c1ac7036764c39a114ba251
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-06-14 21:48:21 -0700

    Added User Todos functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Header } from './components';
-import { getUsers } from './api';
+import { 
+  Header,
+  UserPosts,
+  UserTodos
+} from './components';
+import { 
+  getUsers,
+  getPostsByUser,
+  getTodosByUser
+} from './api';
 
 
 const App = () => {
   const [userList, setUserList] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
+  const [userTodos, setUserTodos] = useState([]);
 
   useEffect(() => {
     getUsers()
@@ -17,6 +27,30 @@ const App = () => {
         console.error(error)
       });
   }, []);
+
+  useEffect(() => {
+    if (!currentUser) {
+      setUserPosts([]);
+      setUserTodos([]);
+      return;
+    }
+
+    getPostsByUser(currentUser.id)
+      .then(posts => {
+        setUserPosts(posts);
+      })
+      .catch(error => {
+        console.error(error)
+      })
+
+    getTodosByUser(currentUser.id)
+      .then(todos => {
+        setUserTodos(todos);
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [currentUser]);
     
   return (
     <div id="App">
@@ -24,6 +58,19 @@ const App = () => {
         userList={ userList }
         currentUser={ currentUser }
         setCurrentUser={ setCurrentUser } />
+      {
+        currentUser
+          ? <div>
+              <UserPosts
+                userPosts={ userPosts }
+                currentUser={ currentUser } />
+              <UserTodos
+                userTodos={ userTodos }
+                currentUser={ currentUser } />
+            </div>
+          : null
+      }
+
     </div>
   );
 }
